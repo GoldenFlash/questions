c33f0ac9977dbc11e67a28e76d6c8ce102c7a953
wangwei <892458829@qq.com>
2017-12-30 14:07:42 +0800

    add

--- 0009.js
+++ 0009.js
@@ -1,18 +1,16 @@
 const obj={a:1,b:2,c:3};
-function foo(o,arr){
-	//请实现该方法
-}
+// function foo(o,arr){
+// 	//请实现该方法
+// }
 
 // foo(obj,["a","c"]);
 // 输出 {a:1,c:3}
 function foo(obj,arr){
-	var q1 = arr[0]
-	var q2 = arr[1]
-	console.log(q1)
-	
-	let {arr[0]:a,q2:c} = obj
-	console.log(a)
-	let o = {q1:a,q2:c}
-	console.log(o)
+	let {p1,p2} = obj;
+	var o ={};
+	o[arr[0]] = p1;
+	o[arr[1]] = p2;
+ 	// console.log()
+ 	return o
 }
-foo(obj,["a","c"]);
+console.log(foo(obj,["a","c"]));
